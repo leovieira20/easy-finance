4cd136f246ece8bd8da6848e5dae3957cc48f97b
Leonardo Vieira <leovieira20@gmail.com>
2022-01-24 21:29:13 +0000

    chore: run project with tye and dapr
    - Add logs with Seq
    - Add telemetry with Zipkin

--- Web/Controllers/WeatherForecastController.cs
+++ Web/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.LogInformation("Fetching forecasts");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,15 +1,27 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var seqServerProtocol = builder.Configuration["SERVICE:SEQ:PROTOCOL"];
+var seqServerHost = builder.Configuration["SERVICE:SEQ:HOST"];
+var seqServerPort = builder.Configuration["SERVICE:SEQ:PORT"];
+var seqServerUrl = $"{seqServerProtocol}://{seqServerHost}:{seqServerPort}";
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.Console()
+    .WriteTo.Seq(seqServerUrl)
+    .Enrich.WithProperty("ApplicationName", "EasyFinance.Web")
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
