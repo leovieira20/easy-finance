d62da260b1c6570f9998cbb5ed31368f9b22093c
Leonardo Vieira <leovieira20@gmail.com>
2022-03-29 12:10:02 +0100

    chore: upgrade nuget packages

--- EasyFinance.Application.Tests.Acceptance/Features/BankAccount/Transactions/RegisterDepositToBankAccount.feature.cs
+++ EasyFinance.Application.Tests.Acceptance/Features/BankAccount/Transactions/RegisterDepositToBankAccount.feature.cs
@@ -25,7 +25,7 @@ namespace EasyFinance.Application.Tests.Acceptance.Features.BankAccount.Transact
         
         private static TechTalk.SpecFlow.ITestRunner testRunner;
         
-        private string[] _featureTags = new string[] {
+        private static string[] featureTags = new string[] {
                 "bankAccount"};
         
         private Xunit.Abstractions.ITestOutputHelper _testOutputHelper;
@@ -42,8 +42,7 @@ namespace EasyFinance.Application.Tests.Acceptance.Features.BankAccount.Transact
         public static void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Features/BankAccount/Transactions", "RegisterDepositToBankAccount", null, ProgrammingLanguage.CSharp, new string[] {
-                        "bankAccount"});
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Features/BankAccount/Transactions", "RegisterDepositToBankAccount", null, ProgrammingLanguage.CSharp, featureTags);
             testRunner.OnFeatureStart(featureInfo);
         }
         
@@ -53,27 +52,27 @@ namespace EasyFinance.Application.Tests.Acceptance.Features.BankAccount.Transact
             testRunner = null;
         }
         
-        public virtual void TestInitialize()
+        public void TestInitialize()
         {
         }
         
-        public virtual void TestTearDown()
+        public void TestTearDown()
         {
             testRunner.OnScenarioEnd();
         }
         
-        public virtual void ScenarioInitialize(TechTalk.SpecFlow.ScenarioInfo scenarioInfo)
+        public void ScenarioInitialize(TechTalk.SpecFlow.ScenarioInfo scenarioInfo)
         {
             testRunner.OnScenarioInitialize(scenarioInfo);
             testRunner.ScenarioContext.ScenarioContainer.RegisterInstanceAs<Xunit.Abstractions.ITestOutputHelper>(_testOutputHelper);
         }
         
-        public virtual void ScenarioStart()
+        public void ScenarioStart()
         {
             testRunner.OnScenarioStart();
         }
         
-        public virtual void ScenarioCleanup()
+        public void ScenarioCleanup()
         {
             testRunner.CollectScenarioErrors();
         }
@@ -86,25 +85,15 @@ namespace EasyFinance.Application.Tests.Acceptance.Features.BankAccount.Transact
         [Xunit.SkippableFactAttribute(DisplayName="Trying to register a deposit to non-existent account")]
         [Xunit.TraitAttribute("FeatureTitle", "RegisterDepositToBankAccount")]
         [Xunit.TraitAttribute("Description", "Trying to register a deposit to non-existent account")]
-        public virtual void TryingToRegisterADepositToNon_ExistentAccount()
+        public void TryingToRegisterADepositToNon_ExistentAccount()
         {
             string[] tagsOfScenario = ((string[])(null));
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Trying to register a deposit to non-existent account", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Trying to register a deposit to non-existent account", null, tagsOfScenario, argumentsOfScenario, featureTags);
 #line 6
         this.ScenarioInitialize(scenarioInfo);
 #line hidden
-            bool isScenarioIgnored = default(bool);
-            bool isFeatureIgnored = default(bool);
-            if ((tagsOfScenario != null))
-            {
-                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((this._featureTags != null))
-            {
-                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((isScenarioIgnored || isFeatureIgnored))
+            if ((TagHelper.ContainsIgnoreTag(tagsOfScenario) || TagHelper.ContainsIgnoreTag(featureTags)))
             {
                 testRunner.SkipScenario();
             }
@@ -127,25 +116,15 @@ namespace EasyFinance.Application.Tests.Acceptance.Features.BankAccount.Transact
         [Xunit.SkippableFactAttribute(DisplayName="Register deposit to existing account")]
         [Xunit.TraitAttribute("FeatureTitle", "RegisterDepositToBankAccount")]
         [Xunit.TraitAttribute("Description", "Register deposit to existing account")]
-        public virtual void RegisterDepositToExistingAccount()
+        public void RegisterDepositToExistingAccount()
         {
             string[] tagsOfScenario = ((string[])(null));
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Register deposit to existing account", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Register deposit to existing account", null, tagsOfScenario, argumentsOfScenario, featureTags);
 #line 13
         this.ScenarioInitialize(scenarioInfo);
 #line hidden
-            bool isScenarioIgnored = default(bool);
-            bool isFeatureIgnored = default(bool);
-            if ((tagsOfScenario != null))
-            {
-                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((this._featureTags != null))
-            {
-                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((isScenarioIgnored || isFeatureIgnored))
+            if ((TagHelper.ContainsIgnoreTag(tagsOfScenario) || TagHelper.ContainsIgnoreTag(featureTags)))
             {
                 testRunner.SkipScenario();
             }
